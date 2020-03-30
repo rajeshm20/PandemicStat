bb0ea5c99564258acacfe024592a7e2e48ba48b2
Rajesh M <rajeshm79@outlook.com>
2020-03-30 04:29:28 -0700

    Initial Commit

---
+++ PandemicStat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PandemicStat
+//
+//  Created by RNSS on 30/03/20.
+//  Copyright © 2020 Rajesh M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
